3cd1095cd48733d125f26e6a4f7893d4267d337f
Amrita <amritajoya@gmail.com>
2026-09-11 00:40:06 +0530

    add a file

---
+++ auth/src/app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const authRoutes = require('./routes/auth.routes')
+const cookieParser = require('cookie-parser')
+const postRoutes = require('./routes/post.routes')
+
+const app = express()
+app.use(express.json())
+app.use(cookieParser())
+
+app.use('/api/auth', authRoutes)
+app.use('/api/post',postRoutes)
+
+module.exports = app
---
+++ auth/src/routes/auth.routes.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const authController = require('../controllers/auth.controller')
+
+const router = express.Router()
+
+router.post('/register',(authController.registerUser))
+
+router.get('/test',(req,res) => {
+    console.log('Cookies: ',req.cookies)
+    res.json({
+        message: 'Test route',
+        cookies: req.cookies
+    })
+})
+
+module.exports = router
